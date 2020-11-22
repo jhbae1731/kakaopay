bfb413ed805cb121261e63f08fdbdaacb8c926f5
JAEHAN BAE <jhbae1731@gmail.com>
2020-11-22 16:58:58 +0900

    과제

--- kakaopay.bat
+++ kakaopay.bat
@@ -1,3 +1,3 @@
 @echo "카카오페이 돈 뿌리기 기능 실행"
 
-start "kakaopay" java -Xmx1048m -Xms512m -XX:+UseParallelGC -jar .\target\kakaopay-0.0.1-SNAPSHOT.jar
+start "kakaopay" java -Xmx1048m -Xms512m -XX:+UseParallelGC -jar .\build\kakaopay-0.0.1-SNAPSHOT.jar
